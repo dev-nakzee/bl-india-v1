6b0c038306b59b3dd81f8065c3f05c2d46788e5c
nakzee <104338457+dev-nakzee@users.noreply.github.com>
2024-06-19 11:04:27 +0530

    Update app.blade.php

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>BL-India : </title>
+        <title>BL-India</title>
 
         @viteReactRefresh
         @vite('resources/js/App/app.jsx')
