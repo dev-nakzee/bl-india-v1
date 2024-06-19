74ee5c5901107e345a863e80a340bd5c78590527
nakzee <104338457+dev-nakzee@users.noreply.github.com>
2024-06-19 13:13:39 +0530

    Create LayoutController.php

---
+++ app/Http/Controllers/fe/LayoutController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\fe;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Sticker;
+use App\Models\SocialMedia;
+use App\Models\Contact;
+
+class LayoutController extends Controller
+{
+    //
+}
