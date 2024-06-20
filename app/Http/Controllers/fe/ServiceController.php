a744dc3b16fb308ebaf9fb6bff2a975486eb8798
nakzee <104338457+dev-nakzee@users.noreply.github.com>
2024-06-20 13:29:22 +0530

    Update ServiceController.php

--- app/Http/Controllers/fe/ServiceController.php
+++ app/Http/Controllers/fe/ServiceController.php
@@ -5,13 +5,14 @@ namespace App\Http\Controllers\fe;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Service;
+use App\Models\ServiceCategory;
 use Illuminate\Http\JsonResponse;
 
 class ServiceController extends Controller
 {
     public function services(Request $request, $subdomain = null): JsonResponse
     {
-        $query = Service::orderBy('order');
+        $query = Service::orderBy('id', 'asc');
 
         // Check if the subdomain is 'global' and filter services accordingly
         if ($subdomain === 'global') {
@@ -20,6 +21,12 @@ class ServiceController extends Controller
 
         $services = $query->get();
 
-        return response()->json($services);
+        // Get the service categories
+        $serviceCategories = ServiceCategory::all();
+
+        return response()->json([
+           'services' => $services,
+           'serviceCategories' => $serviceCategories,
+        ]);
     }
 }
