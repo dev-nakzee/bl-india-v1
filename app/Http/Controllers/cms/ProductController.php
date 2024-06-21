d87cd3b4230c87d867842fa1c9dcbf0b2c0128a2
nakzee <104338457+dev-nakzee@users.noreply.github.com>
2024-06-21 12:34:03 +0530

    commit

--- app/Http/Controllers/cms/ProductController.php
+++ app/Http/Controllers/cms/ProductController.php
@@ -18,7 +18,7 @@ class ProductController extends Controller
     public function index(): JsonResponse
     {
         //
-        $products = Product::with('productCategory', 'services')->orderBy('id')->get();
+        $products = Product::with('productCategory', 'services')->orderBy('id', 'desc')->get();
         return response()->json($products);
     }
 
