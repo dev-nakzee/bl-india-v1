02c4eee8f58f0d97e8d64e8edbaa9ec99de25d8c
nakzee <104338457+dev-nakzee@users.noreply.github.com>
2024-07-02 13:49:10 +0530

    Update api.jsx

--- resources/js/Site/Services/api.jsx
+++ resources/js/Site/Services/api.jsx
@@ -10,12 +10,25 @@ const getBaseURL = () => {
   return 'http://localhost:8000/api/v1/fe'; // Default to local
 };
 
+const getLocale = () => {
+  return localStorage.getItem('locale') || 'en'; // Default to 'en' if locale is not set
+};
+
 const apiClient = axios.create({
   baseURL: getBaseURL(),
   headers: {
     'Content-Type': 'application/json',
     'Accept': 'application/json',
   },
+});
+
+apiClient.interceptors.request.use((config) => {
+  const locale = getLocale();
+  config.headers['Accept-Language'] = locale;
+  config.headers['current-locale'] = locale;
+  return config;
+}, (error) => {
+  return Promise.reject(error);
 });
 
 export default apiClient;
