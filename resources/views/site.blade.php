a47efea2e8e79ebe3b1f8d6650d2f391b246ac87
nakzee <104338457+dev-nakzee@users.noreply.github.com>
2024-07-03 22:17:17 +0530

    Update site.blade.php

--- resources/views/site.blade.php
+++ resources/views/site.blade.php
@@ -8,8 +8,6 @@
 
         <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;700&family=Raleway:wght@300;400;500;700&family=Roboto:wght@300;400;500;700&display=swap" rel="stylesheet">
 
-        <link rel="preconnect" href="https://fonts.googleapis.com">        
-
         @viteReactRefresh
         @vite('resources/js/Site/app.jsx')
         @vite('resources/css/app.css')
