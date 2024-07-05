8a46d0ba11912acc6589b9b3cc4583ab12c06c6a
nakzee <104338457+dev-nakzee@users.noreply.github.com>
2024-07-05 17:56:41 +0530

    Update DownloadController.php

--- app/Http/Controllers/fe/DownloadController.php
+++ app/Http/Controllers/fe/DownloadController.php
@@ -8,6 +8,8 @@ use App\Models\Page;
 use App\Models\DownloadCategory;
 use App\Models\Download;
 use Illuminate\Http\JsonResponse;
+use Stichoza\GoogleTranslate\GoogleTranslate;
+use DOMDocument;
 
 class DownloadController extends Controller
 {
@@ -29,5 +31,31 @@ class DownloadController extends Controller
             'downloadCategories' => $downloadCategories,
             'downloads' => $downloads
         ]);
+    }
+
+    private function translateText($text)
+    {
+        return $text ? $this->translator->translate($text) : '';
+    }
+
+    private function translateHtmlContent($html)
+    {
+        if (is_null($html)) {
+            return '';
+        }
+
+        $doc = new DOMDocument();
+        libxml_use_internal_errors(true);
+        $doc->loadHTML($html, LIBXML_HTML_NOIMPLIED | LIBXML_HTML_NODEFDTD);
+        libxml_clear_errors();
+
+        $xpath = new \DOMXPath($doc);
+        $textNodes = $xpath->query('//text()');
+
+        foreach ($textNodes as $textNode) {
+            $textNode->nodeValue = $this->translateText($textNode->nodeValue);
+        }
+
+        return $doc->saveHTML();
     }
 }
