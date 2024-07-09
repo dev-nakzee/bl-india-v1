bfab3f77946280adaaae55f1a1297b35352e193c
nakzee <104338457+dev-nakzee@users.noreply.github.com>
2024-07-09 16:19:31 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,33 +2,32 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
 
-
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.jsx'],
+            input: ['resources/css/app.css', 'resources/js/Site/app.jsx', 'resources/js/App/app.jsx', 'resources/js/app.jsx'],
             refresh: true,
         }),
         react(),
     ],
-    // build: {
-    //     outDir: 'dist',
-    //     minify: 'esbuild',
-    //     sourcemap: false,
-    //     chunkSizeWarningLimit: 500,
-    //     rollupOptions: {
-    //     output: {
-    //         manualChunks(id) {
-    //         if (id.includes('node_modules')) {
-    //             return id.toString().split('node_modules/')[1].split('/')[0].toString();
-    //         }
-    //         },
-    //     },
-    //     },
-    //     cssCodeSplit: true,
-    // },
-    // optimizeDeps: {
-    //     include: ['react', 'react-dom'],
-    //     exclude: ['js-big-decimal']
-    // }
+    build: {
+        outDir: 'public/build', // Change this to 'public/build'
+        minify: 'esbuild',
+        sourcemap: false,
+        chunkSizeWarningLimit: 500,
+        rollupOptions: {
+            output: {
+                manualChunks(id) {
+                    if (id.includes('node_modules')) {
+                        return id.toString().split('node_modules/')[1].split('/')[0].toString();
+                    }
+                },
+            },
+        },
+        cssCodeSplit: true,
+    },
+    optimizeDeps: {
+        include: ['react', 'react-dom'],
+        exclude: ['js-big-decimal']
+    }
 });
