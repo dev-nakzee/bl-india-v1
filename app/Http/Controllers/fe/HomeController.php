b87958b01b84f078340d293d9230d728b4dbb069
nakzee <104338457+dev-nakzee@users.noreply.github.com>
2024-07-13 02:38:23 +0530

    Update HomeController.php

--- app/Http/Controllers/fe/HomeController.php
+++ app/Http/Controllers/fe/HomeController.php
@@ -14,7 +14,6 @@ use App\Models\Sticker;
 use Illuminate\Http\JsonResponse;
 use Stichoza\GoogleTranslate\GoogleTranslate;
 use Illuminate\Support\Facades\Cache;
-use Illuminate\Support\Facades\Log;
 
 class HomeController extends Controller
 {
@@ -175,12 +174,7 @@ class HomeController extends Controller
 
         $cacheKey = 'translated_text_' . md5($text);
         return Cache::remember($cacheKey, 60 * 60 * 24, function () use ($text) {
-            try {
-                return $this->translator->translate($text);
-            } catch (\Exception $e) {
-                Log::error('Translation error: ' . $e->getMessage());
-                return $text; // Fallback to original text if translation fails
-            }
+            return $this->translator->translate($text);
         });
     }
 }
