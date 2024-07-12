40c8dbcc00f6c579c437a7bcb3d19f8791c32bbd
nakzee <104338457+dev-nakzee@users.noreply.github.com>
2024-07-12 13:22:22 +0530

    Update SharePage.jsx

--- resources/js/Site/Components/SharePage.jsx
+++ resources/js/Site/Components/SharePage.jsx
@@ -1,49 +1,25 @@
 import React from 'react';
-import { IconButton, Menu, MenuItem } from '@mui/material';
+import { IconButton, Menu, MenuItem, Box } from '@mui/material';
 import ShareIcon from '@mui/icons-material/Share';
+import FacebookIcon from '@mui/icons-material/Facebook';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import TelegramIcon from '@mui/icons-material/Telegram';
+import WhatsAppIcon from '@mui/icons-material/WhatsApp';
+import LinkedinIcon from '@mui/icons-material/LinkedIn';
+import PinterestIcon from '@mui/icons-material/Pinterest';
+import RedditIcon from '@mui/icons-material/Reddit';
+// import ViberIcon from '@mui/icons-material/Viber';
+import EmailIcon from '@mui/icons-material/Email';
 import {
   FacebookShareButton,
-  FacebookMessengerShareButton,
   TwitterShareButton,
   TelegramShareButton,
   WhatsappShareButton,
   LinkedinShareButton,
   PinterestShareButton,
-  VKShareButton,
-  OKShareButton,
   RedditShareButton,
-  TumblrShareButton,
-  MailruShareButton,
-  LivejournalShareButton,
   ViberShareButton,
-  WorkplaceShareButton,
-  LineShareButton,
-  WeiboShareButton,
-  PocketShareButton,
-  InstapaperShareButton,
-  HatenaShareButton,
   EmailShareButton,
-  FacebookIcon,
-  FacebookMessengerIcon,
-  TwitterIcon,
-  TelegramIcon,
-  WhatsappIcon,
-  LinkedinIcon,
-  PinterestIcon,
-  VKIcon,
-  OKIcon,
-  RedditIcon,
-  TumblrIcon,
-  MailruIcon,
-  LivejournalIcon,
-  ViberIcon,
-  WorkplaceIcon,
-  LineIcon,
-  WeiboIcon,
-  PocketIcon,
-  InstapaperIcon,
-  HatenaIcon,
-  EmailIcon,
 } from 'react-share';
 
 const SharePage = ({ color = 'default' }) => {
@@ -75,112 +51,79 @@ const SharePage = ({ color = 'default' }) => {
         anchorEl={anchorEl}
         open={Boolean(anchorEl)}
         onClose={handleClose}
+        PaperProps={{
+          style: {
+            maxHeight: 300, // Adjust height if necessary
+            width: 'auto',
+            // padding: '10px',
+            backgroundColor: 'rgba(13, 98, 154, 0.8)', // Semi-transparent background
+          },
+        }}
       >
-        <MenuItem onClick={handleClose}>
-          <FacebookShareButton url={shareUrl} quote={title}>
-            <FacebookIcon size={32} round />
-          </FacebookShareButton>
-        </MenuItem>
-        <MenuItem onClick={handleClose}>
-          <FacebookMessengerShareButton url={shareUrl} appId="YOUR_APP_ID">
-            <FacebookMessengerIcon size={32} round />
-          </FacebookMessengerShareButton>
-        </MenuItem>
-        <MenuItem onClick={handleClose}>
-          <TwitterShareButton url={shareUrl} title={title}>
-            <TwitterIcon size={32} round />
-          </TwitterShareButton>
-        </MenuItem>
-        <MenuItem onClick={handleClose}>
-          <TelegramShareButton url={shareUrl} title={title}>
-            <TelegramIcon size={32} round />
-          </TelegramShareButton>
-        </MenuItem>
-        <MenuItem onClick={handleClose}>
-          <WhatsappShareButton url={shareUrl} title={title} separator=":: ">
-            <WhatsappIcon size={32} round />
-          </WhatsappShareButton>
-        </MenuItem>
-        <MenuItem onClick={handleClose}>
-          <LinkedinShareButton url={shareUrl} title={title} summary={description} source={shareUrl}>
-            <LinkedinIcon size={32} round />
-          </LinkedinShareButton>
-        </MenuItem>
-        <MenuItem onClick={handleClose}>
-          <PinterestShareButton url={shareUrl} media={image}>
-            <PinterestIcon size={32} round />
-          </PinterestShareButton>
-        </MenuItem>
-        <MenuItem onClick={handleClose}>
-          <VKShareButton url={shareUrl} image={image}>
-            <VKIcon size={32} round />
-          </VKShareButton>
-        </MenuItem>
-        <MenuItem onClick={handleClose}>
-          <OKShareButton url={shareUrl} title={title}>
-            <OKIcon size={32} round />
-          </OKShareButton>
-        </MenuItem>
-        <MenuItem onClick={handleClose}>
-          <RedditShareButton url={shareUrl} title={title}>
-            <RedditIcon size={32} round />
-          </RedditShareButton>
-        </MenuItem>
-        <MenuItem onClick={handleClose}>
-          <TumblrShareButton url={shareUrl} title={title} caption={description}>
-            <TumblrIcon size={32} round />
-          </TumblrShareButton>
-        </MenuItem>
-        <MenuItem onClick={handleClose}>
-          <MailruShareButton url={shareUrl} title={title}>
-            <MailruIcon size={32} round />
-          </MailruShareButton>
-        </MenuItem>
-        <MenuItem onClick={handleClose}>
-          <LivejournalShareButton url={shareUrl} title={title}>
-            <LivejournalIcon size={32} round />
-          </LivejournalShareButton>
-        </MenuItem>
-        <MenuItem onClick={handleClose}>
-          <ViberShareButton url={shareUrl} title={title}>
-            <ViberIcon size={32} round />
-          </ViberShareButton>
-        </MenuItem>
-        <MenuItem onClick={handleClose}>
-          <WorkplaceShareButton url={shareUrl} quote={title}>
-            <WorkplaceIcon size={32} round />
-          </WorkplaceShareButton>
-        </MenuItem>
-        <MenuItem onClick={handleClose}>
-          <LineShareButton url={shareUrl} title={title}>
-            <LineIcon size={32} round />
-          </LineShareButton>
-        </MenuItem>
-        <MenuItem onClick={handleClose}>
-          <WeiboShareButton url={shareUrl} title={title} image={image}>
-            <WeiboIcon size={32} round />
-          </WeiboShareButton>
-        </MenuItem>
-        <MenuItem onClick={handleClose}>
-          <PocketShareButton url={shareUrl} title={title}>
-            <PocketIcon size={32} round />
-          </PocketShareButton>
-        </MenuItem>
-        <MenuItem onClick={handleClose}>
-          <InstapaperShareButton url={shareUrl} title={title} description={description}>
-            <InstapaperIcon size={32} round />
-          </InstapaperShareButton>
-        </MenuItem>
-        <MenuItem onClick={handleClose}>
-          <HatenaShareButton url={shareUrl} title={title}>
-            <HatenaIcon size={32} round />
-          </HatenaShareButton>
-        </MenuItem>
-        <MenuItem onClick={handleClose}>
-          <EmailShareButton url={shareUrl} subject={title} body={description}>
-            <EmailIcon size={32} round />
-          </EmailShareButton>
-        </MenuItem>
+        <Box
+          sx={{
+            display: 'grid',
+            gridTemplateColumns: 'repeat(3, 1fr)',
+            gap: 0,
+          }}
+        >
+          <MenuItem onClick={handleClose}>
+            <FacebookShareButton url={shareUrl} quote={title}>
+              <IconButton>
+                <FacebookIcon sx={{ color: 'white' }} />
+              </IconButton>
+            </FacebookShareButton>
+          </MenuItem>
+          <MenuItem onClick={handleClose}>
+            <TwitterShareButton url={shareUrl} title={title}>
+              <IconButton>
+                <TwitterIcon sx={{ color: 'white' }} />
+              </IconButton>
+            </TwitterShareButton>
+          </MenuItem>
+          <MenuItem onClick={handleClose}>
+            <TelegramShareButton url={shareUrl} title={title}>
+              <IconButton>
+                <TelegramIcon sx={{ color: 'white' }} />
+              </IconButton>
+            </TelegramShareButton>
+          </MenuItem>
+          <MenuItem onClick={handleClose}>
+            <WhatsappShareButton url={shareUrl} title={title} separator=":: ">
+              <IconButton>
+                <WhatsAppIcon sx={{ color: 'white' }} />
+              </IconButton>
+            </WhatsappShareButton>
+          </MenuItem>
+          <MenuItem onClick={handleClose}>
+            <LinkedinShareButton url={shareUrl} title={title} summary={description} source={shareUrl}>
+              <IconButton>
+                <LinkedinIcon sx={{ color: 'white' }} />
+              </IconButton>
+            </LinkedinShareButton>
+          </MenuItem>
+          <MenuItem onClick={handleClose}>
+            <PinterestShareButton url={shareUrl} media={image}>
+              <IconButton>
+                <PinterestIcon sx={{ color: 'white' }} />
+              </IconButton>
+            </PinterestShareButton>
+          </MenuItem>
+          <MenuItem onClick={handleClose}>
+            <RedditShareButton url={shareUrl} title={title}>
+              <IconButton>
+                <RedditIcon sx={{ color: 'white' }} />
+              </IconButton>
+            </RedditShareButton>
+          </MenuItem>
+          <MenuItem onClick={handleClose}>
+            <EmailShareButton url={shareUrl} subject={title} body={description}>
+              <IconButton>
+                <EmailIcon sx={{ color: 'white' }} />
+              </IconButton>
+            </EmailShareButton>
+          </MenuItem>
+        </Box>
       </Menu>
     </>
   );
