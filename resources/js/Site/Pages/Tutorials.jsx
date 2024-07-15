17101900665db32aca196c8f4e60951e337483ab
nakzee <104338457+dev-nakzee@users.noreply.github.com>
2024-07-15 13:38:41 +0530

    Update Tutorials.jsx

--- resources/js/Site/Pages/Tutorials.jsx
+++ resources/js/Site/Pages/Tutorials.jsx
@@ -6,13 +6,21 @@ import {
   CardMedia,
   Typography,
   Container,
-  CircularProgress
+  CircularProgress,
+  Dialog,
+  DialogContent,
+  DialogTitle,
+  IconButton,
+  Box
 } from '@mui/material';
 import apiClient from '../Services/api'; // Ensure this is your configured axios instance
+import CloseIcon from '@mui/icons-material/Close';
 
 const Tutorials = () => {
   const [tutorials, setTutorials] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [open, setOpen] = useState(false);
+  const [selectedTutorial, setSelectedTutorial] = useState(null);
 
   useEffect(() => {
     fetchTutorials();
@@ -29,6 +37,16 @@ const Tutorials = () => {
     }
   };
 
+  const handleClickOpen = (tutorial) => {
+    setSelectedTutorial(tutorial);
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+    setSelectedTutorial(null);
+  };
+
   return (
     <Container sx={{ marginTop: 4 }}>
       <Typography variant="h4" gutterBottom>
@@ -40,7 +58,7 @@ const Tutorials = () => {
         <Grid container spacing={4}>
           {tutorials.map((tutorial) => (
             <Grid item xs={12} sm={6} md={4} key={tutorial.id}>
-              <Card>
+              <Card onClick={() => handleClickOpen(tutorial)}>
                 <CardMedia
                   component="iframe"
                   height="200"
@@ -57,6 +75,46 @@ const Tutorials = () => {
             </Grid>
           ))}
         </Grid>
+      )}
+
+      {selectedTutorial && (
+        <Dialog open={open} onClose={handleClose} maxWidth="md" fullWidth>
+          <DialogTitle>
+            {selectedTutorial.title}
+            <IconButton
+              aria-label="close"
+              onClick={handleClose}
+              sx={{
+                position: 'absolute',
+                right: 8,
+                top: 8,
+                color: (theme) => theme.palette.grey[500],
+              }}
+            >
+              <CloseIcon />
+            </IconButton>
+          </DialogTitle>
+          <DialogContent>
+            <Box display="flex" flexDirection={{ xs: 'column', md: 'row' }}>
+              <Box flex="1" marginRight={{ md: 2 }}>
+                <iframe
+                  width="100%"
+                  height="315"
+                  src={selectedTutorial.video_url}
+                  title={selectedTutorial.title}
+                  frameBorder="0"
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                  allowFullScreen
+                ></iframe>
+              </Box>
+              <Box flex="1">
+                <Typography variant="body1" color="textPrimary">
+                  <div dangerouslySetInnerHTML={{ __html: selectedTutorial.description }} />
+                </Typography>
+              </Box>
+            </Box>
+          </DialogContent>
+        </Dialog>
       )}
     </Container>
   );
