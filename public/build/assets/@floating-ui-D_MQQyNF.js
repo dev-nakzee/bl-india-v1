29b9038f8c1eb20ce4ffca69ca7a70f5a1dada62
nakzee <104338457+dev-nakzee@users.noreply.github.com>
2024-07-22 22:35:09 +0530

    commit

--- public/build/assets/@floating-ui-D_MQQyNF.js
+++
@@ -1,1 +0,0 @@
-import"./react-CO8DTg_N.js";import"./react-dom-IHzYg9H9.js";
--- public/build/assets/form-data-DuqYhENU.js
+++
@@ -1,1 +0,0 @@
-import{g as o}from"./@babel-DEAgQchq.js";var r=typeof self=="object"?self.FormData:window.FormData;const a=o(r);export{a as F};
