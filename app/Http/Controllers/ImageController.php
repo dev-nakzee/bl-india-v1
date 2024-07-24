8e2ae20c4ce0f6984590e70da55345b6c1792cf8
nakzee <104338457+dev-nakzee@users.noreply.github.com>
2024-07-25 00:04:49 +0530

    commit

---
+++ app/Http/Controllers/ImageController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\Response;
+use Spatie\ImageOptimizer\OptimizerChainFactory;
+
+class ImageController extends Controller
+{
+    //
+    public function show($path)
+    {
+        return Cache::remember("images/{$path}", 60 * 24 * 7, function () use ($path) {
+            if (Storage::disk('public')->exists($path)) {
+                $optimizerChain = OptimizerChainFactory::create();
+                $filePath = storage_path('app/public/' . $path);
+                $optimizerChain->optimize($filePath);
+
+                $file = Storage::disk('public')->get($path);
+                $type = Storage::disk('public')->mimeType($path);
+
+                return Response::make($file, 200)->header("Content-Type", $type);
+            } else {
+                abort(404);
+            }
+        });
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,13 @@
 <?php
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ImageController;
 
 // CMS Routes
 Route::get('/cms/{path?}', function () {
     return view('app'); // Your CMS React view file
 })->where('path', '.*');
+
+Route::get('storage/{path}', [ImageController::class, 'show'])->where('path', '.*');
 
 // Site Routes
 Route::get('/{any?}', function () {
