6fed6bbdfb16cab200b649202aa6751ffad3d9c3
nakzee <104338457+dev-nakzee@users.noreply.github.com>
2024-08-08 12:26:04 +0530

    Update SearchController.php

--- app/Http/Controllers/fe/SearchController.php
+++ app/Http/Controllers/fe/SearchController.php
@@ -60,44 +60,44 @@ class SearchController extends Controller
                 ->with('category')
                 ->get();
             
-                $results['standards'] = ProductServiceMap::where('is', 'LIKE', "%$query%")
-                ->orWhere('others', 'LIKE', "%$query%")
-                ->orWhere('scheme', 'LIKE', "%$query%")
-                ->orWhere('group', 'LIKE', "%$query%")
-                ->with(['product' => function($query) {
-                        $query->select('id', 'name', 'slug')
-                              ->with(['services' => function($query) {
-                                  $query->select('id', 'name', 'slug', 'service_category_id')
-                                        ->with(['serviceCategory' => function($query) {
-                                            $query->select('id', 'slug');
-                                        }]);
-                              }]);
-                    },
-                    'service' => function($query) {
-                        $query->select('id', 'name', 'slug', 'service_category_id')
-                              ->with(['serviceCategory' => function($query) {
-                                  $query->select('id', 'slug');
-                              }]);
-                    }])
-                ->get()
-                ->map(function($map) {
-                    return [
-                        'product' => [
-                            'id' => $map->product->id,
-                            'name' => $map->product->name,
-                            'slug' => $map->product->slug,
-                            'category_slug' => $map->product->services->map(function($service) {
-                                return $service->serviceCategory->slug;
-                            })
-                        ],
-                        'service' => [
-                            'id' => $map->service->id,
-                            'name' => $map->service->name,
-                            'slug' => $map->service->slug,
-                            'category_slug' => $map->service->serviceCategory->slug
-                        ]
-                    ];
-                });
+  // Search standards and include detailed product and service info
+$results['standards'] = ProductServiceMap::where('is', 'LIKE', "%$query%")
+->orWhere('others', 'LIKE', "%$query%")
+->orWhere('scheme', 'LIKE', "%$query%")
+->orWhere('group', 'LIKE', "%$query%")
+->with(['product' => function($query) {
+        $query->select('products.id', 'products.name', 'products.slug')
+              ->with(['services' => function($query) {
+                  $query->select('services.id', 'services.name', 'services.slug', 'services.service_category_id')
+                        ->with(['serviceCategory' => function($query) {
+                            $query->select('service_categories.id', 'service_categories.slug');
+                        }]);
+              }]);
+    },
+    'service' => function($query) {
+        $query->select('services.id', 'services.name', 'services.slug', 'services.service_category_id')
+              ->with(['serviceCategory' => function($query) {
+                  $query->select('service_categories.id', 'service_categories.slug');
+              }]);
+    }])
+->get()
+->map(function($map) {
+    return [
+        'product' => [
+            'id' => $map->product->id,
+            'name' => $map->product->name,
+            'slug' => $map->product->slug,
+            'category_slugs' => $map->product->services->pluck('serviceCategory.slug')->unique()
+        ],
+        'service' => [
+            'id' => $map->service->id,
+            'name' => $map->service->name,
+            'slug' => $map->service->slug,
+            'category_slug' => $map->service->serviceCategory->slug
+        ]
+    ];
+});
+
             
 
         }
