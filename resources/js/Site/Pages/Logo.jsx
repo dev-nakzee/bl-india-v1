caf4c777a0baeecf913d574ab0d285b7e5e497cb
nakzee <104338457+dev-nakzee@users.noreply.github.com>
2024-09-04 16:10:37 +0530

    Revert "Logo url"
    This reverts commit f938b85fc0f647d8d5576a83fa4a590a62ef1846.

--- resources/js/Site/Pages/Logo.jsx
+++ resources/js/Site/Pages/Logo.jsx
@@ -1,44 +1,23 @@
 import React from 'react';
 
-// Array of logos where each entry contains the base name of the logo
-const logos = [
-    { name: '200x48', jpg: '/logos/logo-200x48.jpg', cdr: '/logos/cdr-200x48.cdr' },
-    // Add more logos as needed
-];
+// Dynamically import all .jpg files from the `logos` folder in `src`
+const importAll = (r) => {
+    return r.keys().map(r);
+};
+
+// Load all jpg images from the `logos` folder
+const logos = importAll(require.context('../logos', false, /\.(jpg|jpeg)$/));
 
 const Logo = () => {
     return (
-        <div style={{ maxWidth: '600px', margin: '0 auto', padding: '20px' }}>
+        <div style={{ display: 'flex', flexWrap: 'wrap', gap: '10px' }}>
             {logos.map((logo, index) => (
-                <div key={index} style={{ marginBottom: '20px', textAlign: 'center' }}>
-                    {/* Display JPG Image */}
-                    <img 
-                        src={logo.jpg} 
-                        alt={logo.name} 
-                        style={{ width: '100%', maxWidth: '400px', height: 'auto' }} 
-                    />
-                    
-                    {/* Download Options */}
-                    <div style={{ marginTop: '10px' }}>
-                        <Button
-                            component="a"
-                            href={logo.jpg}
-                            download
-                            variant="contained"
-                            style={{ marginRight: '10px' }}
-                        >
-                            Download JPG
-                        </Button>
-                        <Button
-                            component="a"
-                            href={logo.cdr}
-                            download
-                            variant="contained"
-                        >
-                            Download CDR
-                        </Button>
-                    </div>
-                </div>
+                <img 
+                    key={index} 
+                    src={logo} 
+                    alt={`Logo ${index}`} 
+                    style={{ width: '100px', height: '100px', objectFit: 'contain' }} 
+                />
             ))}
         </div>
     );
